63c2289c7915a7592f6706a65bedf09370474948
mrdotiendat <mrdotiendat@gmail.com>
2026-04-14 15:23:57 +0700

    fix

--- config.dev.js
+++ config.dev.js
@@ -1,5 +1,5 @@
 window.DTD_NEWS_CONFIG_DEV = {
   supabaseUrl: "https://ppzlxtkcitkumrteuemz.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwemx4dGtjaXRrdW1ydGV1ZW16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNDU3NjUsImV4cCI6MjA5MTcyMTc2NX0.6H2PCIlkCIyMFXDD6CvzlKLjqFGzsmYCG5tFqn9vIq0",
-  ingestSecret: "",
+  ingestSecret: "datdeptrai",
 };
